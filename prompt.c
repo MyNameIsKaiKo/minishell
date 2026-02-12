1976ff150a8a6c4897eeda706d9d7967ba8e700e
Nabil Redouane <nredouan@3D4.42angouleme.fr>
2026-02-12 15:04:11 +0100

    prompt just started, signals handled

---
+++ prompt.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prompt.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nredouan <nredouan@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 15:18:50 by nredouan          #+#    #+#             */
+/*   Updated: 2026/02/12 15:02:43 by nredouan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void	handler(int signal)
+{
+	if (signal == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	size_t	size;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	size = ft_strlen(s1) + ft_strlen(s2);
+	dest = malloc(size * sizeof(char) + 1);
+	if (!dest)
+		return (NULL);
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		dest[i + j] = s2[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+int	main(void)
+{
+	char	*str;
+	char	*tmp;
+	char	cwd[256];
+
+	signal(SIGINT, handler);
+	signal(SIGQUIT, SIG_IGN);
+	getcwd(cwd, sizeof(cwd));
+	str = ft_strjoin("\033[35m<T&J minishell>", cwd);
+	tmp = str;
+	str = ft_strjoin(str, "$ \033[0m");
+	free (tmp);
+	tmp = readline(str);
+	printf("%s\n", tmp);
+	free(str);
+}
