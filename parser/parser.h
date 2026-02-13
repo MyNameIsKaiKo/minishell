28658f2fce020f12feaa465f5966eaa5fd250de0
Jean-Baptiste Leray <jleray@3C3.42angouleme.fr>
2026-02-13 15:15:48 +0100

    Added the first row of parsing (header and struct)

---
+++ parser/parser.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jleray <marvin@d42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/13 14:54:04 by jleray            #+#    #+#             */
+/*   Updated: 2026/02/13 14:54:04 by jleray           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+
+typedef struct s_node
+{
+	char			*type;
+	char			*data;
+	struct s_node	*right;
+	struct s_node	*left;
+}					t_node;
+
+#endif
