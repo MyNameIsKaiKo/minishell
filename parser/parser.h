4139ead4ffac928e60929572f3468f8e681bbd9a
Jean-Baptiste Leray <jleray@3C3.42angouleme.fr>
2026-02-13 15:40:55 +0100

    add things to the .h

--- parser/parser.h
+++ parser/parser.h
@@ -13,12 +13,15 @@
 #ifndef PARSER_H
 # define PARSER_H
 
+# include "../include/libft/libft.h"
+
 typedef struct s_node
 {
 	char			*type;
 	char			*data;
 	struct s_node	*right;
 	struct s_node	*left;
+	struct s_node	*head;
 }					t_node;
 
 #endif
