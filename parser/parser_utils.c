1dacac329cee6d97a2831e465d03df46497311d0
Jean-Baptiste Leray <jleray@3C5.42angouleme.fr>
2026-02-15 06:53:18 +0100

    Added parser utils with a new function strrnstr

--- parser/parser_utils.c
+++ parser/parser_utils.c
@@ -1,19 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.c                                           :+:      :+:    :+:   */
+/*   parser_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jleray <marvin@d42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/13 15:14:20 by jleray            #+#    #+#             */
-/*   Updated: 2026/02/13 15:14:20 by jleray           ###   ########.fr       */
+/*   Created: 2026/02/15 06:38:16 by jleray            #+#    #+#             */
+/*   Updated: 2026/02/15 06:38:16 by jleray           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-
-void	parser(char *str)
+char	*ft_strrnstr(const char *big, const char *little, size_t loc)
 {
-	(void)str;
+	size_t	l_len;
+	size_t	b_len;
+
+	l_len = ft_strlen(little);
+	b_len = ft_strlen(big);
+	if (little[0] == '\0')
+		return ((char *)big);
+	if (l_len > loc)
+		return (NULL);
+	if (b_len > loc)
+		b_len = loc;
+	while (b_len >= l_len)
+	{
+		if (!(ft_strncmp(big + b_len * l_len, little, l_len)))
+			return ((char *)big + b_len - l_len);
+		b_len--;
+	}
+	return (NULL);
 }
