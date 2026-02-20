5b07d9b0dc2da68bf57f62ddb5947fe98994bac5
Jean-Baptiste Leray <jleray@3A5.42angouleme.fr>
2026-02-20 22:01:14 +0100

    created an enum in order to store every type I though bout "

---
+++ parser/lexer.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jleray <marvin@d42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/20 20:16:19 by jleray            #+#    #+#             */
+/*   Updated: 2026/02/20 20:16:19 by jleray           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+// Aim -> create t_lexer with everything with a token
+// token list :
+// - word -> file and cmd ?
+// - operator -> logic or pipe
+// - direction
+
+
--- parser/parser.h
+++ parser/parser.h
@@ -15,49 +15,62 @@
 
 # include "../include/libft/libft.h"
 
+typedef enum e_token_type
+{
+	WORD,		 // cat, file.txt etc..
+	PIPE,		 // |
+	REDIR_IN,	 // <
+	REDIR_OUT,	 // >
+	APPEND,		 // >>
+	HEREDOC, 	// <<
+	PONCT, 		// (), '' or ""
+	OPERATOR 	// && or ||
+}						t_token_type;
+
 typedef struct s_lexer
 {
-	char			*data;
-	char			*type;
-	struct s_lexer	*next;
-}					t_lexer;
+	char				*data;
+	enum e_token_type	*type;
+	struct s_lexer		*next;
+}						t_lexer;
 
 typedef struct s_node
 {
-	char			*type;
-	char			*data;
-	struct s_node	*right;
-	struct s_node	*left;
-	struct s_node	*head;
-}					t_node;
+	char				*type;
+	char				*data;
+	struct s_node		*right;
+	struct s_node		*left;
+	struct s_node		*head;
+}						t_node;
 
 // -- parser Function --
-t_node				*make_tree(char *str, t_node **head);
-int					exec_tree(t_node *tree);
+t_node					*make_tree(char *str, t_node **head);
+int						exec_tree(t_node *tree);
 
 // -- node Function --
-t_node				*nodenew(char *data, char *type, t_node *head);
-void				node_add(t_node **node, t_node *new_node, char *side);
-void				node_free(t_node *node);
+t_node					*nodenew(char *data, char *type, t_node *head);
+void					node_add(t_node **node, t_node *new_node, char *side);
+void					node_free(t_node *node);
 
 // -- operator_utils Function --
-char				*or_and(char *op);
-char				*get_last_op(char *str, char *type);
+char					*or_and(char *op);
+char					*get_last_op(char *str, char *type);
 
 // -- parser_utils Function --
-t_node				*create_treenodes(char *str, char *is_op, t_node **head);
-int					pipe_exec(t_node *tree);
+t_node					*create_treenodes(char *str, char *is_op,
+							t_node **head);
+int						pipe_exec(t_node *tree);
 
 // -- string_utils Function --
-char				*ft_strrnstr(const char *big, const char *little,
-						size_t loc);
-char				*ft_getleft(char *s, char *del);
-char				*ft_getright(char *s, char *del);
+char					*ft_strrnstr(const char *big, const char *little,
+							size_t loc);
+char					*ft_getleft(char *s, char *del);
+char					*ft_getright(char *s, char *del);
 
 // minish_pipex Function --
-int					pipe_exec(t_node *tree);
+int						pipe_exec(t_node *tree);
 
 // minish_cmd Function --
-int					cmd_exec(t_node *tree);
+int						cmd_exec(t_node *tree);
 
 #endif
