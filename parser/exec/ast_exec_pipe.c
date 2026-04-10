d9afbdf04496886941ac090c8a783ea946e2ea47
Jean-Baptiste Leray <jleray@3C5.42angouleme.fr>
2026-04-10 19:26:58 +0200

    fixed all fd

--- parser/exec/ast_exec_pipe.c
+++ parser/exec/ast_exec_pipe.c
@@ -6,7 +6,7 @@
 /*   By: jleray <marvin@d42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/03 15:57:13 by jleray            #+#    #+#             */
-/*   Updated: 2026/04/09 15:47:54 by jleray           ###   ########.fr       */
+/*   Updated: 2026/04/10 18:54:14 by jleray           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,10 @@ void	handle_first(t_ast *tree, t_data data, int pipefd[2])
 		close(pipefd[1]);
 		exit(127);
 	}
-	else if (data.filesfd.fdin > 2)
-	{
-		dup2(data.filesfd.fdin, STDIN_FILENO);
-		close(data.filesfd.fdin);
-	}
+	dup2(data.filesfd.fdin, STDIN_FILENO);
 	dup2(pipefd[1], STDOUT_FILENO);
+	if (data.filesfd.fdin > 2)
+		close(data.filesfd.fdin);
 	close(pipefd[0]);
 	close(pipefd[1]);
 	exit(exec_tree(tree->left, data));
@@ -40,11 +38,9 @@ void	handle_scd(t_ast *tree, t_data data, int pipefd[2])
 		exit(127);
 	}
 	dup2(pipefd[0], STDIN_FILENO);
-	if (data.filesfd.fdout > 2)
-	{
-		dup2(data.filesfd.fdout, STDOUT_FILENO);
+	dup2(data.filesfd.fdout, STDOUT_FILENO);
+	if (data.filesfd.fdin > 2)
 		close(data.filesfd.fdout);
-	}
 	close(pipefd[0]);
 	close(pipefd[1]);
 	exit(exec_tree(tree->right, data));
@@ -67,8 +63,10 @@ int	exec_pipe(t_ast *tree, t_data data)
 		handle_scd(tree, data, pipefd);
 	close(pipefd[0]);
 	close(pipefd[1]);
-	close(data.filesfd.fdin);
-	close(data.filesfd.fdout);
+	if (data.filesfd.fdin > 2)
+		close(data.filesfd.fdin);
+	if (data.filesfd.fdout > 2)
+		close(data.filesfd.fdout);
 	waitpid(first_child, NULL, 0);
 	waitpid(scd_child, &status, 0);
 	if (WIFEXITED(status))
