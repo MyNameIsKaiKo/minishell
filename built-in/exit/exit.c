ad22eca34ced6945c8de70397f22b3dd86464ce5
Nabil Redouane <nredouan@3A7.42angouleme.fr>
2026-04-12 17:56:33 +0200

    fix exit

--- built-in/exit/exit.c
+++ built-in/exit/exit.c
@@ -6,7 +6,7 @@
 /*   By: nredouan <nredouan@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 14:55:54 by nredouan          #+#    #+#             */
-/*   Updated: 2026/04/12 16:27:45 by nredouan         ###   ########.fr       */
+/*   Updated: 2026/04/12 17:20:31 by nredouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,23 @@
 
 static bool	exit_verif(char *args)
 {
-	int	i;
+	bool	is_negative;
+	int		i;
 
 	i = 0;
+	is_negative = false;
+	if (args && (args[0] == '-' || args[0] == '+'))
+	{
+		is_negative = true;
+		i++;
+	}
 	while (args && args[i])
 	{
 		if (!ft_isdigit(args[i]))
 			return (false);
 		i++;
 	}
-	if (i > 20)
+	if ((i > 19 && !is_negative) || (i > 20 && is_negative))
 		return (false);
 	return (true);
 }
@@ -32,14 +39,12 @@ int	minish_exit(char **args, t_env *env_var)
 {
 	int	i;
 	
-	i = 0;
-	while (args[i])
-		i++;
+	i = -1;
+	while (args[++i]);
 	if (i > 1)
 	{
 		ft_putendl_fd("minishell: exit: too many arguments", 2);
 		env_var->exit_status = 1;
-		//ne dois pas exit minishell
 		return (env_var->exit_status);
 	}
 	printf("exit\n");
@@ -47,13 +52,13 @@ int	minish_exit(char **args, t_env *env_var)
 	{
 		ft_putstr_fd("minishell: exit: ", 2);
 		ft_putstr_fd(args[0], 2);
-		ft_putendl_fd(":numeric argument required", 2);
+		ft_putendl_fd(": numeric argument required", 2);
 		env_var->exit_status =  2;
 	}
 	else if (args[0])
 		env_var->exit_status = ft_atoll(args[0]) % 256;
+	if (env_var->exit_status < 0)
+		env_var->exit_status *= -1;
 	return (env_var->exit_status);
 	//doit exit minishell
 }
-
-//si pas d'arguments, exit status = le dernier reçu (par exemple si ctrl + C est le dernier exit status, "exit" sans rien revient a faire "exit 130")
