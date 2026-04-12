46350baabc915ac9be265e94aba403fefc838537
jleray <59555781+MyNameIsKaiKo@users.noreply.github.com>
2026-04-12 16:50:29 +0200

    git add corrected return on operator

--- parser/exec/ast_exec.c
+++ parser/exec/ast_exec.c
@@ -20,14 +20,14 @@ int	exec_operator(t_ast *tree, t_data data)
 	{
 		output = exec_tree(tree->left, data);
 		if (!output)
-			exec_tree(tree->right, data);
+			output = exec_tree(tree->right, data);
 		return (output);
 	}
 	else if (!ft_strncmp(tree->data, "||", 2))
 	{
 		output = exec_tree(tree->left, data);
 		if (output)
-			exec_tree(tree->right, data);
+			output = exec_tree(tree->right, data);
 		return (output);
 	}
 	return (1);
