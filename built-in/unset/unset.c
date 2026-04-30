44fa1a64bed939578e11fd2329164e13f7f86ad6
Nabil Redouane <nredouan@3G7.42angouleme.fr>
2026-04-30 20:06:42 +0200

    unset first node fix

--- built-in/unset/unset.c
+++ built-in/unset/unset.c
@@ -6,11 +6,23 @@
 /*   By: nredouan <nredouan@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 15:16:42 by nredouan          #+#    #+#             */
-/*   Updated: 2026/04/24 17:24:30 by nredouan         ###   ########.fr       */
+/*   Updated: 2026/04/30 20:05:11 by nredouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../built_in.h"
+
+static void	recup_info_head(t_env *to_recup, t_env *new_head)
+{
+	if (to_recup != new_head)
+	{
+		new_head->exec = to_recup->exec;
+		new_head->exit_status = to_recup->exit_status;
+		new_head->is_valid_exit = to_recup->is_valid_exit;
+		new_head->prompt = to_recup->prompt;
+		new_head->pwd_s = to_recup->pwd_s;
+	}
+}
 
 static t_env	*supp_node(const char *name, t_env *env_var)
 {
@@ -29,6 +41,7 @@ static t_env	*supp_node(const char *name, t_env *env_var)
 	{
 		head = env_var->next;
 		head->prev = NULL;
+		recup_info_head(env_var, head);
 	}
 	else
 	{
